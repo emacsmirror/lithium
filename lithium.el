78fce2eb7c703849cd26b8ab7368e5db4bd191b8
Siddhartha <sid@countvajhula.com>
2024-07-23 17:21:37 -0700

    Basic macros for defining a Lithium mode
    Wrap Emacs's minor modes with additional configuration to create modes
    whose keybindings take precedence over regular minor mode bindings.
    These modes also implicitly define entry and exit hooks whose
    semantics are appropriate for modal interfaces in the Vim sense.
    
    Like Hydra, keybinding definitions allow some flexibility on exiting
    the mode.
    
    As a formal modal interface, we intend to provide clear hooks at every
    point in the mode and command lifecycle so that modal functionality
    can be reasoned about easily as well as extended.

---
+++ lithium.el
@@ -0,0 +1,91 @@
+;;; lithium.el --- Lightweight modal interfaces -*- lexical-binding: t -*-
+
+;; URL: https://github.com/countvajhula/lithium
+
+;; This program is "part of the world," in the sense described at
+;; http://drym.org.  From your perspective, this is no different than
+;; MIT or BSD or other such "liberal" licenses that you may be
+;; familiar with, that is to say, you are free to do whatever you like
+;; with this program.  It is much more than BSD or MIT, however, in
+;; that it isn't a license at all but an idea about the world and how
+;; economic systems could be set up so that everyone wins.  Learn more
+;; at drym.org.
+;;
+;; This work transcends traditional legal and economic systems, but
+;; for the purposes of any such systems within which you may need to
+;; operate:
+;;
+;; This is free and unencumbered software released into the public domain.
+;; The authors relinquish any copyright claims on this work.
+;;
+
+;;; Commentary:
+;;
+;; Lightweight modal interfaces
+;;
+
+;;; Code:
+
+(defun lithium--define-key (keyspec keymap mode)
+  "Helper to define an individual key according to spec.
+
+Parse the KEYSPEC to define the key in KEYMAP for MODE.
+
+KEYSPEC is expected to be (key action [exit]). If `exit' is missing,
+then it's an ordinary binding of key to action, but if exit is present
+and set to true, then also exit the MODE after performing the action."
+  (if (and (> (length keyspec) 2) (caddr keyspec))
+      (define-key keymap
+        (kbd (car keyspec))
+        `(lambda ()
+           (interactive)
+           (funcall ',(cadr keyspec))
+           (funcall ,mode -1)))
+    (define-key keymap
+      (kbd (car keyspec))
+      (cadr keyspec))))
+
+(defmacro lithium-keymap (spec mode)
+  "Specify a keymap for the MODE.
+
+SPEC is the set of keybinding specifications."
+  `(let ((keymap (make-sparse-keymap)))
+     (dolist (keyspec (quote ,spec))
+       (lithium--define-key keyspec keymap ',mode))
+     keymap))
+
+(defmacro lithium-define-mode (name
+                               docstring
+                               keymap
+                               &rest
+                               body)
+  "Define a lithium mode."
+  (declare (indent defun))
+  `(progn
+
+     (defvar ,(intern (concat (symbol-name name) "-entry-hook")) nil
+       ,(concat "Entry hook for" (symbol-name name) "."))
+     (defvar ,(intern (concat (symbol-name name) "-exit-hook")) nil
+       ,(concat "Exit hook for" (symbol-name name) "."))
+
+     (define-minor-mode ,name
+       ,docstring
+       :global t
+       :keymap (lithium-keymap ,keymap ',name)
+       ,@body
+       (if ,name
+           (run-hooks
+            (quote ,(intern
+                     (concat (symbol-name name)
+                             "-entry-hook"))))
+         (run-hooks
+          (quote ,(intern
+                   (concat (symbol-name name)
+                           "-exit-hook"))))))
+
+     (add-to-list 'emulation-mode-map-alists
+                  (let ((keymap ,(intern
+                                  (concat
+                                   (symbol-name name)
+                                   "-map"))))
+                    (list (cons (quote ,name) keymap))))))
